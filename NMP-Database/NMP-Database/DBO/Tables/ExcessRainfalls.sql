592668ad9dd044f36505c75c90350699474ef50c
Anil Yadav <Anil.Yadav@RSK-BSL.COM>
2024-03-27 13:12:46 +0530

    Database project created

---
+++ NMP-Database/NMP-Database/DBO/Tables/ExcessRainfalls.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ExcessRainfalls]
+(
+    [FarmID] INT NOT NULL ,
+	[Year] INT NOT NULL, 
+    [ExcessRainfall] INT NULL CONSTRAINT DF_ExcessRainfalls_ExcessRainfall DEFAULT 0, 
+    [WinterRainfall] INT NULL,
+    CONSTRAINT [PK_ExcessRainfall] PRIMARY KEY CLUSTERED ([FarmID],[Year] ASC),    
+    CONSTRAINT [FK_ExcessRainfall_Farms] FOREIGN KEY([FarmID]) REFERENCES [dbo].[Farms] ([ID])
+);
+
---
+++ NMP-Database/NMP-Database/DBO/Tables/Farms.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE [dbo].[Farms]
+(
+	[ID]                 INT NOT NULL IDENTITY(1,1),
+	[Name]               NVARCHAR (50)   NOT NULL,
+    [Address1]           NVARCHAR (50)   NULL,
+    [Address2]           NVARCHAR (50)   NULL,
+    [Address3]           NVARCHAR (50)   NULL,
+    [Address4]           NVARCHAR (50)   NULL,
+    [PostCode]           NVARCHAR (50)   NOT NULL,
+    [CPH]                NVARCHAR (50)   NULL,
+    [FarmerName]         NVARCHAR (128)  NULL,
+    [BusinessName]       NVARCHAR (128)  NULL,
+    [SBI]                NVARCHAR (20)   NULL,
+    [STD]                NVARCHAR (6)    NOT NULL,
+    [Telephone]          NVARCHAR (15)   NULL,
+    [Mobile]             NVARCHAR (13)   NULL,
+    [Email]              NVARCHAR (256)  NULL,
+    [Rainfall]           INT             NULL,
+    [TotalFarmArea]      DECIMAL (18, 4) NULL CONSTRAINT DF_Farms_TotalFarmArea DEFAULT 0,
+    [AverageAltitude]    INT             NULL CONSTRAINT DF_Farms_AverageAltitude DEFAULT 0,
+    [RegistredOrganicProducer] BIT     NOT NULL CONSTRAINT DF_Farms_RegistredOrganicProducer DEFAULT 0,
+    [MetricUnits]        BIT             NULL CONSTRAINT DF_Farms_MetricUnits DEFAULT 0,
+    [EnglishRules]       BIT             NULL CONSTRAINT DF_Farms_EnglishRules DEFAULT 1,
+    CONSTRAINT [PK_Farms] PRIMARY KEY ([ID] ASC)
+
+
+)
+
