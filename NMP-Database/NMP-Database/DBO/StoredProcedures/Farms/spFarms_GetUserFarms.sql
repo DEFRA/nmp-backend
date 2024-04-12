84e9c1d6d76c7f584a539ecbb6904c9ca4ecb3f0
Shaun Brown <shaun.brown@rsk-bsl.com>
2024-04-12 15:25:12 +0100

    Added unique check for farm name and postcode. added stored procedure to get all farms for user.

---
+++ NMP-Database/NMP-Database/DBO/StoredProcedures/Farms/spFarms_GetUserFarms.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE [dbo].[spFarms_GetUserFarms]
+    @userID INT
+AS
+BEGIN
+    SELECT 
+        [Farms].[Name],
+        [Farms].[Address1],
+        [Farms].[Address2],
+        [Farms].[Address3],
+        [Farms].[Address4],
+        [Farms].[Postcode],
+        [Farms].[CPH],
+        [Farms].[FarmerName],
+        [Farms].[BusinessName],
+        [Farms].[SBI],
+        [Farms].[STD],
+        [Farms].[Telephone],
+        [Farms].[Mobile],
+        [Farms].[Email],
+        [Farms].[Rainfall],
+        [Farms].[TotalFarmArea],
+        [Farms].[AverageAltitude],
+        [Farms].[RegisteredOrganicProducer],
+        [Farms].[MetricUnits],
+        [Farms].[EnglishRules],
+        [Farms].[NVZFields],
+        [Farms].[FieldsAbove300SeaLevel]
+    FROM 
+        [UserFarms]
+    LEFT JOIN 
+        [Farms] ON [Farms].ID = [UserFarms].[FarmID]
+    WHERE 
+        [UserFarms].[UserID] = @userID
+    ORDER BY [Farms].[Name];
+END
