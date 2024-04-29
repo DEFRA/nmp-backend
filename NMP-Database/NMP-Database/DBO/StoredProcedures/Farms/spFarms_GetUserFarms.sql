888e6f9c9bc8f4e3b1d90145a220047562346303
shubham.singhal <shubham.singhal@rsk-bsl.com>
2024-04-29 12:14:31 +0530

    Changed stored procedure get user farms, if short summary condition added

--- NMP-Database/NMP-Database/DBO/StoredProcedures/Farms/spFarms_GetUserFarms.sql
+++ NMP-Database/NMP-Database/DBO/StoredProcedures/Farms/spFarms_GetUserFarms.sql
@@ -1,36 +1,53 @@
 ﻿CREATE PROCEDURE [dbo].[spFarms_GetUserFarms]
-    @userID INT
+    @userID INT,
+    @shortSummary BIT = 0
 AS
 BEGIN
-    SELECT 
-        [Farms].[ID],
-        [Farms].[Name],
-        [Farms].[Address1],
-        [Farms].[Address2],
-        [Farms].[Address3],
-        [Farms].[Address4],
-        [Farms].[Postcode],
-        [Farms].[CPH],
-        [Farms].[FarmerName],
-        [Farms].[BusinessName],
-        [Farms].[SBI],
-        [Farms].[STD],
-        [Farms].[Telephone],
-        [Farms].[Mobile],
-        [Farms].[Email],
-        [Farms].[Rainfall],
-        [Farms].[TotalFarmArea],
-        [Farms].[AverageAltitude],
-        [Farms].[RegisteredOrganicProducer],
-        [Farms].[MetricUnits],
-        [Farms].[EnglishRules],
-        [Farms].[NVZFields],
-        [Farms].[FieldsAbove300SeaLevel]
-    FROM 
-        [UserFarms]
-    LEFT JOIN 
-        [Farms] ON [Farms].ID = [UserFarms].[FarmID]
-    WHERE 
-        [UserFarms].[UserID] = @userID
-    ORDER BY [Farms].[Name];
+    IF @shortSummary = 1
+    BEGIN
+        SELECT 
+            [Farms].[ID],
+            [Farms].[Name]
+        FROM 
+            [UserFarms]
+        LEFT JOIN 
+            [Farms] ON [Farms].ID = [UserFarms].[FarmID]
+        WHERE 
+            [UserFarms].[UserID] = @userID
+        ORDER BY [Farms].[Name];
+    END
+    ELSE 
+    BEGIN
+        SELECT 
+            [Farms].[ID],
+            [Farms].[Name],
+            [Farms].[Address1],
+            [Farms].[Address2],
+            [Farms].[Address3],
+            [Farms].[Address4],
+            [Farms].[Postcode],
+            [Farms].[CPH],
+            [Farms].[FarmerName],
+            [Farms].[BusinessName],
+            [Farms].[SBI],
+            [Farms].[STD],
+            [Farms].[Telephone],
+            [Farms].[Mobile],
+            [Farms].[Email],
+            [Farms].[Rainfall],
+            [Farms].[TotalFarmArea],
+            [Farms].[AverageAltitude],
+            [Farms].[RegisteredOrganicProducer],
+            [Farms].[MetricUnits],
+            [Farms].[EnglishRules],
+            [Farms].[NVZFields],
+            [Farms].[FieldsAbove300SeaLevel]
+        FROM 
+            [UserFarms]
+        LEFT JOIN 
+            [Farms] ON [Farms].ID = [UserFarms].[FarmID]
+        WHERE 
+            [UserFarms].[UserID] = @userID
+        ORDER BY [Farms].[Name];
+    END
 END
