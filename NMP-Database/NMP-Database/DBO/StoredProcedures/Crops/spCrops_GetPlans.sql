3487d8287bcaef4c2ca9d6cea06e73aecab76cbb
shubham.singhal <shubham.singhal@rsk-bsl.com>
2024-05-16 18:11:16 +0530

    Created SP for get plans

---
+++ NMP-Database/NMP-Database/DBO/StoredProcedures/Crops/spCrops_GetPlans.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[spCrops_GetPlans]
+    @farmId INT,
+    @confirm INT
+AS
+BEGIN
+    SELECT
+        [Crops].[Year],
+        CASE
+        WHEN MAX([Crops].[ModifiedOn]) IS NULL THEN MAX([Crops].[CreatedOn])
+        ELSE MAX([Crops].[ModifiedOn])
+    END AS LastModifiedOn
+    FROM
+        [Crops]
+    INNER JOIN
+        [Fields] ON [Fields].[ID] = [Crops].[FieldID]
+    WHERE
+        [Fields].[FarmID] = @farmId
+    AND [Crops].[Confirm] = @confirm
+    GROUP BY 
+        [Crops].[Year]
+END
