0187f879031cebfba2c76052d66845343d4daa39
shubham.singhal <shubham.singhal@rsk-bsl.com>
2024-05-17 12:49:29 +0530

    Created sp for getting plans for selected harvest year

---
+++ NMP-Database/NMP-Database/DBO/StoredProcedures/Crops/spCrops_GetPlansByHarvestYear.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[spCrops_GetPlansByHarvestYear]
+    @farmId INT,
+    @harvestYear INT
+AS
+BEGIN
+    SELECT
+        [Crops].[CropTypeID],
+        [Fields].[ID] AS FieldID,
+        MIN([Fields].[Name]) AS FieldName,
+        CASE
+            WHEN MAX([Crops].[ModifiedOn]) IS NULL THEN MAX([Crops].[CreatedOn])
+            ELSE MAX([Crops].[ModifiedOn])
+            END AS LastModifiedOn
+    FROM
+        [Crops]
+    INNER JOIN
+        [Fields] ON [Fields].[ID] = [Crops].[FieldID]
+    WHERE
+        [Fields].[FarmID] = @farmId
+    AND 
+        [Crops].[Year] = @harvestYear
+    AND 
+        [Crops].[Confirm] = 0
+    GROUP BY 
+        [Crops].[CropTypeID],
+        [Fields].[ID]
+END
