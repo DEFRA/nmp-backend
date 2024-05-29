79bcda2f55915749c3a38b06e2c1c1901819f26b
shubham.singhal <shubham.singhal@rsk-bsl.com>
2024-05-29 16:16:19 +0530

    Added sp to merge organisation and user

---
+++ NMP-Database/NMP-Database/DBO/StoredProcedures/Users/spUsers_MergeUser.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spUsers_MergeUser]
+(
+    @userIdentifier UNIQUEIDENTIFIER,
+    @givenName NVARCHAR(50),
+    @surname NVARCHAR(50),
+    @email NVARCHAR(256)
+)
+AS
+BEGIN
+MERGE INTO Users AS target
+USING (VALUES (@userIdentifier, @givenName, @surname, @email)) AS source (userIdentifier, givenName, surname, email)
+ON target.UserIdentifier = source.userIdentifier
+WHEN MATCHED THEN
+    UPDATE SET GivenName = source.givenName, Surname = source.surname, Email = source.email
+WHEN NOT MATCHED THEN
+    INSERT (UserIdentifier, GivenName, Surname, Email) VALUES (source.userIdentifier, source.givenName, source.surname, source.email);
+END
