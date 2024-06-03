843410c4011c942d908c1d925a3e9c7a9431ad1c
Anil Yadav <Anil.Yadav@RSK-BSL.COM>
2024-06-03 10:03:26 +0530

    Climat datas table code refactor

--- NMP-Database/NMP-Database/DBO/Tables/UserFarms.sql
+++ NMP-Database/NMP-Database/DBO/Tables/UserFarms.sql
@@ -1,10 +1,10 @@
-﻿CREATE TABLE [dbo].[UserFarms]
-(
-	[UserID] INT NOT NULL,
-	[FarmID] INT NOT NULL,
-	[RoleID] INT NOT NULL,
-	CONSTRAINT [PK_UserFarms] PRIMARY KEY ([UserID],[FarmID] ASC),	
-	CONSTRAINT [FK_UserFarms_Users] FOREIGN KEY([UserID]) REFERENCES [dbo].[Users] ([ID]),
-	CONSTRAINT [FK_UserFarms_Farms] FOREIGN KEY([FarmID]) REFERENCES [dbo].[Farms] ([ID]),
-	CONSTRAINT [FK_UserFarms_Roles] FOREIGN KEY([RoleID]) REFERENCES [dbo].[Roles] ([ID])
-)
+﻿--CREATE TABLE [dbo].[UserFarms]
+--(
+--	[UserID] INT NOT NULL,
+--	[FarmID] INT NOT NULL,
+--	[RoleID] INT NOT NULL,
+--	CONSTRAINT [PK_UserFarms] PRIMARY KEY ([UserID],[FarmID] ASC),	
+--	CONSTRAINT [FK_UserFarms_Users] FOREIGN KEY([UserID]) REFERENCES [dbo].[Users] ([ID]),
+--	CONSTRAINT [FK_UserFarms_Farms] FOREIGN KEY([FarmID]) REFERENCES [dbo].[Farms] ([ID]),
+--	CONSTRAINT [FK_UserFarms_Roles] FOREIGN KEY([RoleID]) REFERENCES [dbo].[Roles] ([ID])
+--)
