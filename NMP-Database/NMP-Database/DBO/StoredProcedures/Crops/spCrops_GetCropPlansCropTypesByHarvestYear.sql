499cdca7060bb083bb4f9e38055c4494bda56d07
shubham.singhal <shubham.singhal@rsk-bsl.com>
2024-06-05 18:21:02 +0530

    Added Sp to get crop types and fields for crop plans

---
+++ NMP-Database/NMP-Database/DBO/StoredProcedures/Crops/spCrops_GetCropPlansCropTypesByHarvestYear.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[spCrops_GetCropPlansCropTypesByHarvestYear]
+    @farmId INT,
+    @harvestYear INT
+AS
+BEGIN
+    SELECT
+        [Crops].[CropTypeID]
+    FROM
+        [Crops]
+    INNER JOIN
+        [Fields] ON [Fields].[ID] = [Crops].[FieldID]
+    WHERE
+        [Fields].[FarmID] = @farmId
+    AND [Crops].[Year] = @harvestYear
+    AND [Crops].[Confirm] = 0
+    GROUP BY
+        [Crops].[CropTypeID]
+END
