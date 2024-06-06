6365c83cb3c892c8159392dddfdad981221ee970
shubham.singhal <shubham.singhal@rsk-bsl.com>
2024-06-06 14:08:37 +0530

    Added sp to get management period ids and updated get crop plan fields sp

--- NMP-Database/NMP-Database/DBO/StoredProcedures/Crops/spCrops_GetCropPlansFieldsByHarvestYear.sql
+++ NMP-Database/NMP-Database/DBO/StoredProcedures/Crops/spCrops_GetCropPlansFieldsByHarvestYear.sql
@@ -1,19 +1,36 @@
 ﻿CREATE PROCEDURE [dbo].[spCrops_GetCropPlansFieldsByHarvestYear]
     @farmId INT,
     @harvestYear INT,
-    @cropTypeId INT
+    @cropTypeId INT = NULL
 AS
 BEGIN
-    SELECT
-        [Fields].[ID],
-        [Fields].[Name]
-    FROM
-        [Crops]
-    INNER JOIN
-        [Fields] ON [Fields].[ID] = [Crops].[FieldID]
-    WHERE
-        [Fields].[FarmID] = @farmId
-    AND [Crops].[Year] = @harvestYear
-    AND [Crops].[CropTypeID] = @cropTypeId
-    AND [Crops].[Confirm] = 0
+    IF @cropTypeId IS NOT NULL
+    BEGIN
+        SELECT DISTINCT
+            [Fields].[ID],
+            [Fields].[Name]
+        FROM
+            [Crops]
+        INNER JOIN
+            [Fields] ON [Fields].[ID] = [Crops].[FieldID]
+        WHERE
+            [Fields].[FarmID] = @farmId
+        AND [Crops].[Year] = @harvestYear
+        AND [Crops].[CropTypeID] = @cropTypeId
+        AND [Crops].[Confirm] = 0
+    END
+    ELSE
+    BEGIN
+        SELECT DISTINCT
+            [Fields].[ID],
+            [Fields].[Name]
+        FROM
+            [Crops]
+        INNER JOIN
+            [Fields] ON [Fields].[ID] = [Crops].[FieldID]
+        WHERE
+            [Fields].[FarmID] = @farmId
+        AND [Crops].[Year] = @harvestYear
+        AND [Crops].[Confirm] = 0
+    END
 END
