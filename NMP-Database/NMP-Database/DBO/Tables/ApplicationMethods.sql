bfd2c314443fbebd1872691a02f90a1e485eb9eb
PoojaTaneja1999 <pooja.taneja@RSK-BSL.COM>
2024-06-12 18:37:16 +0530

    Post script for Application method,Incorporation method and Incorporatio delay . and some little modification in these tables also

--- NMP-Database/NMP-Database/DBO/Tables/ApplicationMethods.sql
+++ NMP-Database/NMP-Database/DBO/Tables/ApplicationMethods.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ApplicationMethods]
 (
-	[ID] INT IDENTITY(1,1) NOT NULL, 
+	[ID] INT  NOT NULL, 
     [Name] NVARCHAR(100) NOT NULL ,
 	[ApplicableFor] NVARCHAR(1) NOT NULL ,
 	CONSTRAINT [PK_ApplicationMethods] PRIMARY KEY CLUSTERED ([ID] ASC)
