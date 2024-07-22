87ce1f1e13dd04cf5025104d7e8a1b9bd576f2ca
Santosh Verma <santosh.verma@RSK-BSL.COM>
2024-07-22 15:00:03 +0530

    Added column DefaultYield in CropTypeLinkings table

--- NMP-Database/NMP-Database/DBO/Tables/CropTypeLinkings.sql
+++ NMP-Database/NMP-Database/DBO/Tables/CropTypeLinkings.sql
@@ -2,6 +2,7 @@
     [CropTypeID]       INT NOT NULL,
     [MannerCropTypeID] INT NOT NULL
     CONSTRAINT [PK_CropTypeLinkings] PRIMARY KEY CLUSTERED ([CropTypeID],[MannerCropTypeID] ASC),
+    [DefaultYield] DECIMAL(18, 3) NULL, 
     CONSTRAINT [FK_CropTypeLinkings_MannerCropTypes_MannerCropTypeID] FOREIGN KEY ([MannerCropTypeID]) REFERENCES [dbo].[MannerCropTypes] ([ID])
 
 );
