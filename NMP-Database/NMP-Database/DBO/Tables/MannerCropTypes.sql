07d8045c93e2176beba94308a20bdeb3f47becfd
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2024-10-11 15:41:36 +0530

    tables that were not being used have been removed.

--- NMP-Database/NMP-Database/DBO/Tables/MannerCropTypes.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[MannerCropTypes] (
-    [ID]               INT            NOT NULL,
-    [Name]             NVARCHAR (250) NOT NULL,
-    [Use]              NVARCHAR (50)  NOT NULL,
-    [CropUptakeFactor] INT            NOT NULL, 
-    CONSTRAINT [PK_MannerCropTypes] PRIMARY KEY ([ID])
-);
-
--- NMP-Database/NMP-Database/DBO/Tables/ManureTypeCategories.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[ManureTypeCategories] (
-    [ID]   INT           NOT NULL,
-    [Name] NVARCHAR (250) NOT NULL, 
-    CONSTRAINT [PK_ManureTypeCategories] PRIMARY KEY ([ID])
-);
-
