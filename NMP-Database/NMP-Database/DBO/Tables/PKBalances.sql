c993dc5357bd3c4ec7ea65360b7755a43b4404b4
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2024-11-18 14:26:42 +0530

    added CreatedBy, Modified By column .

--- NMP-Database/NMP-Database/DBO/Tables/PKBalances.sql
+++ NMP-Database/NMP-Database/DBO/Tables/PKBalances.sql
@@ -5,7 +5,13 @@
     [FieldID] INT NOT NULL, 
     [PBalance] INT NOT NULL CONSTRAINT DF_PKBalances_PBalance DEFAULT 0, 
     [KBalance] INT NOT NULL CONSTRAINT DF_PKBalances_KBalance DEFAULT 0,  
+    [CreatedOn]             DATETIME2       NULL DEFAULT GETDATE(), 
+    [CreatedByID]           INT             NULL, 
+    [ModifiedOn]            DATETIME2       NULL, 
+    [ModifiedByID]          INT             NULL,
     [PreviousID]  INT NULL,    
     CONSTRAINT [PK_PKBalances] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_PKBalances_Fields] FOREIGN KEY([FieldID]) REFERENCES [dbo].[Fields] ([ID]),
+    CONSTRAINT [FK_PKBalances_Users_CreatedBy] FOREIGN KEY([CreatedByID]) REFERENCES [dbo].[Users] ([ID]),
+    CONSTRAINT [FK_PKBalances_Users_ModifiedBy] FOREIGN KEY([ModifiedByID]) REFERENCES [dbo].[Users] ([ID]),
 )
