c81fc171e53fe9e9da6417da85b98c5a22ff84b9
Santosh Verma <santosh.verma@RSK-BSL.COM>
2024-12-05 18:51:30 +0530

    tables for Grassland field

---
+++ NMP-Database/NMP-Database/DBO/Tables/GrassTypicalCuts.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[GrassTypicalCuts]
+(
+	[Id] INT NOT NULL IDENTITY(1,1),
+	[Name] NVARCHAR(50) NOT NULL,
+	CONSTRAINT [PK_GrassTypicalCuts] PRIMARY KEY ([ID] ASC),
+)
---
+++ NMP-Database/NMP-Database/DBO/Tables/PreviousGrasses.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[PreviousGrasses]
+(
+	[ID] INT NOT NULL IDENTITY(1,1),
+	[FieldID] INT NOT NULL,
+    [HasGrassInLastThreeYear] BIT NOT NULL,
+    [HarvestYear] INT NULL,
+    [GrassManagementOptionID] INT NULL,
+    [GrassTypicalCutID] INT NULL,
+    [HasGreaterThan30PercentClover] BIT NULL,
+    [SoilNitrogenSupplyItemID] INT NULL,
+	[CreatedOn] DATETIME2 NULL CONSTRAINT DF_PreviousGrasses_CreatedOn DEFAULT GETDATE(), 
+    [CreatedByID] INT NULL, 
+    [ModifiedOn] DATETIME2 NULL, 
+    [ModifiedByID] INT NULL,
+    CONSTRAINT [PK_PreviousGrasses] PRIMARY KEY ([ID] ASC),
+    CONSTRAINT [FK_PreviousGrasses_Fields] FOREIGN KEY([FieldID]) REFERENCES [dbo].[Fields] ([ID]),
+    CONSTRAINT [FK_PreviousGrasses_GrassManagementOptions] FOREIGN KEY([GrassManagementOptionID]) REFERENCES [dbo].[GrassManagementOptions] ([ID]),
+    CONSTRAINT [FK_PreviousGrasses_GrassTypicalCuts] FOREIGN KEY([GrassTypicalCutID]) REFERENCES [dbo].[GrassTypicalCuts] ([ID]),
+    CONSTRAINT [FK_PreviousGrasses_SoilNitrogenSupplyItems] FOREIGN KEY([SoilNitrogenSupplyItemID]) REFERENCES [dbo].[SoilNitrogenSupplyItems] ([ID])
+)
