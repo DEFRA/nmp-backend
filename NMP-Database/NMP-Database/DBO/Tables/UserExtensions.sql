540520a261ce4a51279bf07363fab5b4167d7027
Anil Yadav <Anil.Yadav@RSK-BSL.COM>
2025-02-18 11:53:40 +0530

    User Extension Table

---
+++ NMP-Database/NMP-Database/DBO/Tables/UserExtensions.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[UserExtensions]
+(
+	[UserID] INT NOT NULL PRIMARY KEY,
+	[IsTermsOfUseAccepted] BIT NOT NULL,
+	[DoNotShowAboutThisService] BIT NOT NULL,
+	CONSTRAINT [FK_UserExtensions_Users] FOREIGN KEY([UserID]) REFERENCES [dbo].[Users] ([ID])
+)
