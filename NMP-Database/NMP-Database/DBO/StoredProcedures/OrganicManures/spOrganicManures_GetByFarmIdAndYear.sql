3bf90a58ab1449b5a141c3280d130bfae865fc9c
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2025-04-09 12:47:07 +0530

    added more column in sp

--- NMP-Database/NMP-Database/DBO/StoredProcedures/OrganicManures/spOrganicManures_GetByFarmIdAndYear.sql
+++ NMP-Database/NMP-Database/DBO/StoredProcedures/OrganicManures/spOrganicManures_GetByFarmIdAndYear.sql
@@ -3,7 +3,10 @@
 	@harvestYear int
 AS
 	SELECT fi.id ID,fi.[Name] [Name],CONVERT(DATE,OrganicManures.ApplicationDate) ApplicationDate,OrganicManures.ID AS OrganicManureId,
-OrganicManures.N Nitrogen,OrganicManures.P2O5 P2O5,OrganicManures.MgO MgO,OrganicManures.K2O K2O,OrganicManures.SO3 SO3,OrganicManures.ManureTypeID AS
+OrganicManures.N Nitrogen,OrganicManures.P2O5 P2O5,OrganicManures.MgO MgO,OrganicManures.K2O K2O,OrganicManures.SO3 SO3,
+OrganicManures.DryMatterPercent AS DryMatterPercent,OrganicManures.NH4N AS NH4N,OrganicManures.NO3N AS NO3N,
+OrganicManures.UricAcid AS UricAcid,
+OrganicManures.ManureTypeID AS
 ManureTypeID,OrganicManures.ManagementPeriodID AS ManagementPeriodID
 FROM OrganicManures 
 INNER JOIN ManagementPeriods AS m ON m.id=OrganicManures.ManagementPeriodID
