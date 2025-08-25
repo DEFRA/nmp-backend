528eb4dd462b355c24e0f809f1256ac6a01759c7
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2025-08-25 14:55:49 +0530

    final chanegs

--- NMP-Database/NMP-Database/DBO/Tables/StorageTypes.sql
+++ NMP-Database/NMP-Database/DBO/Tables/StorageTypes.sql
@@ -2,6 +2,6 @@
 (
 	[ID] INT NOT NULL  IDENTITY(1,1),	
 	[Name] NVARCHAR(50) NOT NULL,
-	[FreeBoardheight] DECIMAL(18,2)  NULL,
+	[FreeBoardHeight] DECIMAL(18,2) NOT NULL,
     CONSTRAINT [PK_StorageTypes] PRIMARY KEY ([ID] ASC),
 )
