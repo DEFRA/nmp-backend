f5df4179cc408ebd05cd2be075942ab12e58d7c7
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2025-09-09 14:47:54 +0530

    changes for NMPT-1887

--- NMP-Database/NMP-Database/Script.PostDeployment.sql
+++ NMP-Database/NMP-Database/Script.PostDeployment.sql
@@ -1528,4 +1528,22 @@ GO
 --UPDATE [dbo].[LivestockTypes] SET [Name]=N'1 calf (all categories except veal) youger than 2 months' where [ID]=1
 --END
 
+
+IF EXISTS (SELECT 1 FROM [dbo].[Crops] WHERE [IsBasePlan] IS NULL)
+BEGIN
+
+    UPDATE [dbo].[Crops] SET [IsBasePlan]=1 WHERE [Yield] IS NULL AND [CropInfo1] IS NULL AND [DefoliationSequenceID] IS NULL
+
+    IF EXISTS (SELECT 1 FROM [dbo].[Crops] WHERE CropTypeID!=140 AND ([Yield] IS NOT NULL OR [CropInfo1] IS NOT NULL))
+    BEGIN
+    UPDATE [dbo].[Crops] SET [IsBasePlan]=0 WHERE CropTypeID!=140 AND ([Yield] IS NOT NULL OR [CropInfo1] IS NOT NULL)
+    END
+
+    IF EXISTS (SELECT 1 FROM [dbo].[Crops] WHERE CropTypeID=140 AND [DefoliationSequenceID] IS NOT NULL)
+    BEGIN
+    UPDATE [dbo].[Crops] SET [IsBasePlan]=0 WHERE CropTypeID=140 AND [DefoliationSequenceID] IS NOT NULL
+    END
+
+END
+
 GO -- do not remove this GO
