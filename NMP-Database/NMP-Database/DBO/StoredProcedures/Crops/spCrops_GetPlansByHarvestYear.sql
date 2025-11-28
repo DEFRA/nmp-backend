6fb0331129ec0e8a4347b1fb3b6cc5010c20a11b
Anil Yadav <Anil.Yadav@RSK-BSL.COM>
2025-11-28 18:49:48 +0530

    Build Failed

--- NMP-Database/NMP-Database/DBO/StoredProcedures/Crops/spCrops_GetPlansByHarvestYear.sql
+++ NMP-Database/NMP-Database/DBO/StoredProcedures/Crops/spCrops_GetPlansByHarvestYear.sql
@@ -4,7 +4,7 @@
 AS
 BEGIN
    SELECT
-        [Crops].[ID] AS CropID,               -- Add this line to retrieve Crops.ID
+        [Crops].[ID] AS CropID,               
         [Crops].[CropTypeID],
         [Fields].[ID] AS FieldID,
         [Fields].[Name] AS FieldName,
@@ -21,21 +21,15 @@ BEGIN
 		[Crops].[PotentialCut] AS PotentialCut,
 		[Crops].SwardManagementID AS SwardManagementID,
 		[Crops].DefoliationSequenceID AS DefoliationSequenceID,
-		[Crops].Establishment AS Establishment,
-		[Crops].IsBasePlan AS IsBasePlan,
+		[Crops].Establishment AS Establishment,		
         CASE
             WHEN [Crops].[ModifiedOn] >= [Crops].[CreatedOn] THEN [Crops].[ModifiedOn]
             ELSE [Crops].[CreatedOn]
-        END AS LastModifiedOn
-		--,
-  --      (SELECT COUNT(DISTINCT [OrganicManures].[ID]) FROM [OrganicManures] WHERE [OrganicManures].[ManagementPeriodID] = [ManagementPeriods].[ID]) AS TotalOrganicManures,
-  --      (SELECT COUNT(DISTINCT [FertiliserManures].[ID])  FROM [FertiliserManures]  WHERE [FertiliserManures].[ManagementPeriodID] = [ManagementPeriods].[ID]) AS TotalFertiliserManures
+        END AS LastModifiedOn		
    FROM
         [Crops]
     INNER JOIN
-        [Fields] ON [Fields].[ID] = [Crops].[FieldID]
-    --INNER JOIN
-    --	[ManagementPeriods] ON [ManagementPeriods].[CropID] = [Crops].[ID]
+        [Fields] ON [Fields].[ID] = [Crops].[FieldID]    
     WHERE
         [Fields].[FarmID] = @farmId
     AND 
