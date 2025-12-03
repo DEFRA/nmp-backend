f3a61cc045db918e6fe43b92ee760868906b0e3d
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2025-12-03 17:34:14 +0530

    changes for grass and double cropping

--- NMP-Database/NMP-Database/DBO/StoredProcedures/Crops/spCrops_GetCropPlansManagementPeriodByHarvestYear.sql
+++ NMP-Database/NMP-Database/DBO/StoredProcedures/Crops/spCrops_GetCropPlansManagementPeriodByHarvestYear.sql
@@ -1,11 +1,11 @@
 ﻿CREATE PROCEDURE [dbo].[spCrops_GetCropPlansManagementPeriodByHarvestYear]
     @fieldIds NVARCHAR(MAX),
     @harvestYear INT,
-    @cropTypeId INT = NULL,
+    @cropGroupName NVARCHAR(120) = NULL,
     @cropOrder INT = NULL
 AS
 BEGIN
-    IF @cropTypeId IS NOT NULL
+    IF @cropGroupName IS NOT NULL
     BEGIN
         SELECT DISTINCT
             [ManagementPeriods].[ID]
@@ -16,7 +16,7 @@ BEGIN
         WHERE
             [Crops].[Year] = @harvestYear
             AND [Crops].[FieldID] IN (SELECT value FROM STRING_SPLIT(@fieldIds, ','))
-            AND [Crops].[CropTypeID] = @cropTypeId
+            AND [Crops].[CropGroupName] = @cropGroupName
             AND [Crops].[Confirm] = 0
             AND (@cropOrder IS NULL OR [Crops].[CropOrder] = @cropOrder)
     END
