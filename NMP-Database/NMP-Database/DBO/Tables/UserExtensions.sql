45ea8d23deed39b50745cb8d802f9478776b9ac2
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2026-02-05 17:44:33 +0530

    NMPT2696 done

--- NMP-Database/NMP-Database/DBO/Tables/UserExtensions.sql
+++ NMP-Database/NMP-Database/DBO/Tables/UserExtensions.sql
@@ -3,5 +3,6 @@
 	[UserID] INT NOT NULL PRIMARY KEY,
 	[IsTermsOfUseAccepted] BIT NOT NULL,
 	[DoNotShowAboutThisService] BIT NOT NULL,
+	[DoNotShowAboutManner] BIT  NULL,
 	CONSTRAINT [FK_UserExtensions_Users] FOREIGN KEY([UserID]) REFERENCES [dbo].[Users] ([ID])
 )
