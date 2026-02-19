1bfadfc7d6ef23e2feb70d9af0a219a4338dabbf
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2026-02-19 18:28:03 +0530

    added created column

--- NMP-Database/NMP-Database/DBO/Tables/FarmsNVZ.sql
+++ NMP-Database/NMP-Database/DBO/Tables/FarmsNVZ.sql
@@ -4,6 +4,12 @@
     [FarmID] INT NOT NULL, 
     [NVZProgrammeID] INT NOT NULL,
     [NVZProgrammeName] NVARCHAR(128) NOT NULL,
+    [CreatedOn] DATETIME2 NULL DEFAULT GETDATE(), 
+    [CreatedByID] INT NULL, 
+    [ModifiedOn] DATETIME2 NULL, 
+    [ModifiedByID] INT NULL,
     CONSTRAINT [PK_FarmsNVZ] PRIMARY KEY ([ID] ASC),
     CONSTRAINT [FK_FarmsNVZ_Farms] FOREIGN KEY([FarmID]) REFERENCES [dbo].[Farms] ([ID]),
+    CONSTRAINT [FK_FarmsNVZ_Users_CreatedBy] FOREIGN KEY([CreatedByID]) REFERENCES [dbo].[Users] ([ID]),
+    CONSTRAINT [FK_FarmsNVZ_Users_ModifiedBy] FOREIGN KEY([ModifiedByID]) REFERENCES [dbo].[Users] ([ID]),
 )
