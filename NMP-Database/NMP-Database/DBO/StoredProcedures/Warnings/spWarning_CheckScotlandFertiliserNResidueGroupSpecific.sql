901dc5d15e18577bb93ee8799c500a41d83a8715
shrey2692 <shreyash.chaturvedi@rsk-bsl.com>
2026-04-14 18:44:41 +0530

    Sonarr fix

--- NMP-Database/NMP-Database/DBO/StoredProcedures/Warnings/spWarning_CheckScotlandFertiliserNResidueGroupSpecific.sql
+++ NMP-Database/NMP-Database/DBO/StoredProcedures/Warnings/spWarning_CheckScotlandFertiliserNResidueGroupSpecific.sql
@@ -1,5 +1,4 @@
-﻿
-CREATE PROCEDURE [dbo].[spWarning_CheckScotlandFertiliserNResidueGroupSpecific]
+﻿CREATE PROCEDURE [dbo].[spWarning_CheckScotlandFertiliserNResidueGroupSpecific]
     @FertiliserID INT
 AS
 BEGIN
@@ -12,7 +11,7 @@ BEGIN
         @FieldID INT,
         @FarmID INT,
         @CountryID INT,
-        @IsWithinNVZ BIT = 0,
+        @IsWithinNVZ BIT,
         @ApplicationDate DATE,
         @CropID INT,
         @CropTypeID INT,
@@ -25,8 +24,8 @@ BEGIN
         @ClosedEndDate DATE,
 
         -- Flags
-        @IsFieldScotland BIT = 0,
-        @IsWinterOSR BIT = 0,
+        @IsFieldScotland BIT,
+        @IsWinterOSR BIT,
         @IsWithinClosedPeriod BIT = 0,
         @IsNResidueGroup456 BIT = 0,
 
@@ -34,108 +33,99 @@ BEGIN
         @IsTriggered BIT = 0;
 
     --------------------------------------------------------------------
-    -- Temp Tables
-    --------------------------------------------------------------------
-    DECLARE @ClosedPeriodTable TABLE (ClosedPeriod NVARCHAR(100));
-
-    DECLARE @ClosedPeriodDates TABLE (
-        ClosedPeriod NVARCHAR(100),
-        ClosedStartDate DATE,
-        ClosedEndDate DATE
-    );
-
-    --------------------------------------------------------------------
-    -- 1) Load Fertiliser + Context
+    -- 1) LOAD ALL CONTEXT IN SINGLE QUERY (🔥 removes duplication)
     --------------------------------------------------------------------
     SELECT
         @ApplicationDate = f.ApplicationDate,
         @ManagementPeriodID = f.ManagementPeriodID,
-        @CropID = mp.CropID
+        @CropID = mp.CropID,
+        @FieldID = c.FieldID,
+        @CropTypeID = c.CropTypeID,
+        @FarmID = fld.FarmID,
+        @IsWithinNVZ = ISNULL(fld.IsWithinNVZ,0),
+        @NvzID = fld.NVZProgrammeID,
+        @CountryID = fm.CountryID
     FROM FertiliserManures f
     INNER JOIN ManagementPeriods mp ON f.ManagementPeriodID = mp.ID
+    INNER JOIN Crops c ON mp.CropID = c.ID
+    INNER JOIN Fields fld ON c.FieldID = fld.ID
+    INNER JOIN Farms fm ON fld.FarmID = fm.ID
     WHERE f.ID = @FertiliserID;
 
     SET @HarvestYear = YEAR(@ApplicationDate);
 
-    SELECT
-        @FieldID = c.FieldID,
-        @CropTypeID = c.CropTypeID
-    FROM Crops c
-    WHERE c.ID = @CropID;
-
-    SELECT
-        @IsWithinNVZ = ISNULL(fld.IsWithinNVZ,0),
-        @FarmID = fld.FarmID,
-        @NvzID = fld.NVZProgrammeID
-    FROM Fields fld
-    WHERE fld.ID = @FieldID;
-
-    SELECT
-        @CountryID = fm.CountryID
-    FROM Farms fm
-    WHERE fm.ID = @FarmID;
-
     --------------------------------------------------------------------
-    -- 2) Flags
+    -- 2) FLAGS (single place)
     --------------------------------------------------------------------
     SET @IsFieldScotland = CASE WHEN @CountryID = 2 THEN 1 ELSE 0 END;
     SET @IsWinterOSR = CASE WHEN @CropTypeID = 20 THEN 1 ELSE 0 END;
 
     --------------------------------------------------------------------
-    -- 3) Get NIndex
+    -- 3) NIndex
     --------------------------------------------------------------------
     SELECT TOP 1
         @NIndex = r.NIndex
     FROM Recommendations r
     WHERE r.ManagementPeriodID = @ManagementPeriodID;
 
-    IF @NIndex IN (4,5,6)
-        SET @IsNResidueGroup456 = 1;
+    SET @IsNResidueGroup456 = CASE WHEN @NIndex IN (4,5,6) THEN 1 ELSE 0 END;
 
     --------------------------------------------------------------------
-    -- 4) Get Closed Period
+    -- 4) CLOSED PERIOD (compact version)
     --------------------------------------------------------------------
+    DECLARE @ClosedPeriodTable TABLE (ClosedPeriod NVARCHAR(100));
+
     INSERT INTO @ClosedPeriodTable
     EXEC dbo.spWarning_GetFertiliserManureClosedPeriod
         @CountryId = @CountryID,
         @CropTypeId = @CropTypeID,
         @NvzId = @NvzID;
 
-    SELECT @ClosedPeriod = ClosedPeriod FROM @ClosedPeriodTable;
+    SELECT TOP 1 @ClosedPeriod = ClosedPeriod FROM @ClosedPeriodTable;
 
     --------------------------------------------------------------------
-    -- Convert Closed Period → Dates
+    -- Convert Closed Period → Dates (no extra duplication)
     --------------------------------------------------------------------
+    DECLARE @ClosedPeriodDates TABLE (
+        ClosedStartDate DATE,
+        ClosedEndDate DATE
+    );
+
     INSERT INTO @ClosedPeriodDates
     EXEC dbo.spConvertClosedPeriodTextToDates
         @ClosedPeriodText = @ClosedPeriod,
         @HarvestYear = @HarvestYear;
 
-    SELECT
+    SELECT TOP 1
         @ClosedStartDate = ClosedStartDate,
         @ClosedEndDate = ClosedEndDate
     FROM @ClosedPeriodDates;
 
     --------------------------------------------------------------------
-    -- 5) Check Application within Closed Period
+    -- 5) Closed Period Check
     --------------------------------------------------------------------
-    IF @ApplicationDate BETWEEN @ClosedStartDate AND @ClosedEndDate
-        SET @IsWithinClosedPeriod = 1;
+    SET @IsWithinClosedPeriod =
+        CASE 
+            WHEN @ApplicationDate BETWEEN @ClosedStartDate AND @ClosedEndDate THEN 1 
+            ELSE 0 
+        END;
 
     --------------------------------------------------------------------
     -- 6) Final Trigger Logic
     --------------------------------------------------------------------
-    IF @IsFieldScotland = 1
-       AND @IsWithinNVZ = 1
-       AND @IsWinterOSR = 1
-       AND @IsWithinClosedPeriod = 1
-       AND @IsNResidueGroup456 = 1
-    BEGIN
-        SET @IsTriggered = 1;
-    END
+    SET @IsTriggered =
+        CASE 
+            WHEN @IsFieldScotland = 1
+             AND @IsWithinNVZ = 1
+             AND @IsWinterOSR = 1
+             AND @IsWithinClosedPeriod = 1
+             AND @IsNResidueGroup456 = 1
+            THEN 1
+            ELSE 0
+        END;
 
     --------------------------------------------------------------------
-    -- 7) OUTPUT (DEBUG FRIENDLY)
+    -- 7) OUTPUT
     --------------------------------------------------------------------
     SELECT
         @IsTriggered AS IsTriggered,
