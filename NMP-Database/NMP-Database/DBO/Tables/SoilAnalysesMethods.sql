3ffe4edd188ffb4dcb2a79600cc14a86d53fb6d9
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2026-04-22 17:37:39 +0530

    remove soil analysis method  table

--- NMP-Database/NMP-Database/DBO/Tables/SoilAnalysesMethods.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[SoilAnalysesMethods]
-(
-    [ID] INT NOT NULL IDENTITY(1,1),
-	[Name] NVARCHAR(50) NOT NULL,	
-    CONSTRAINT [PK_SoilAnalysesMethods] PRIMARY KEY ([ID] ASC),
-)
