212222fdb8551fa3a44d0b7ef7c67f35425c47db
Pooja <pooja.taneja@rsk-bsl.com>
2026-06-17 11:23:30 +0530

    changes for manner

--- NMP-Database/NMP-Database/DBO/Tables/MannerEstimationFinancialValues.sql
+++ NMP-Database/NMP-Database/DBO/Tables/MannerEstimationFinancialValues.sql
@@ -5,29 +5,24 @@
     [NitrogenValue] INT NOT NULL,
     [PhosphateValue] INT NOT NULL,
     [PotashValue] INT NOT NULL,
-
     [NitrogenProductId] INT NOT NULL,
     [PhosphateProductId] INT NOT NULL,
     [PotashProductId] INT NOT NULL,
-
     [NitrogenProductName] NVARCHAR(100) NOT NULL,
     [PhosphateProductName] NVARCHAR(100) NOT NULL,
     [PotashProductName] NVARCHAR(100) NOT NULL,
-
     [NitrogenProductPrice] INT NOT NULL,
     [PhosphateProductPrice] INT NOT NULL,
     [PotashProductPrice] INT NOT NULL,
-
     [NitrogenPrice] INT NOT NULL,
     [PhosphatePrice] INT NOT NULL,
     [PotashPrice] INT NOT NULL,
-
     [CreatedOn]             DATETIME2       NULL DEFAULT GETDATE(), 
     [CreatedByID]           INT             NULL,
     [ModifiedOn]            DATETIME2       NULL,
     [ModifiedByID]          INT             NULL,
-    CONSTRAINT [PK_MannerFinancialValues] PRIMARY KEY CLUSTERED ([ID] ASC),
-    CONSTRAINT [FK_MannerFinancialValues_MannerEstimationApplications] FOREIGN KEY ([MannerEstimationApplicationID]) REFERENCES [dbo].[MannerEstimationApplications] ([ID]),
-    CONSTRAINT [FK_MannerFinancialValues_Users_CreatedBy] FOREIGN KEY ([CreatedByID]) REFERENCES [dbo].[Users] ([ID]),
-    CONSTRAINT [FK_MannerFinancialValuesj_Users_ModifiedBy] FOREIGN KEY ([ModifiedByID]) REFERENCES [dbo].[Users] ([ID])
+    CONSTRAINT [PK_MannerEstimationFinancialValues] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_MannerEstimationFinancialValues_MannerEstimationApplications] FOREIGN KEY ([MannerEstimationApplicationID]) REFERENCES [dbo].[MannerEstimationApplications] ([ID]),
+    CONSTRAINT [FK_MannerEstimationFinancialValues_Users_CreatedBy] FOREIGN KEY ([CreatedByID]) REFERENCES [dbo].[Users] ([ID]),
+    CONSTRAINT [FK_MannerEstimationFinancialValues_Users_ModifiedBy] FOREIGN KEY ([ModifiedByID]) REFERENCES [dbo].[Users] ([ID])
 )
