adc7b0cfa6f1dbd7052783c5330b4e81225576ca
Pooja <pooja.taneja@rsk-bsl.com>
2026-07-30 15:33:27 +0530

    Changes in pre

--- NMP-Database/NMP-Database/Script.PreDeployment.sql
+++ NMP-Database/NMP-Database/Script.PreDeployment.sql
@@ -36,5 +36,16 @@ BEGIN
     END
 END
 
+--30-07-2026
+IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Recommendations' AND TABLE_SCHEMA = 'dbo')
+BEGIN   
+            UPDATE Recommendations
+            SET IsSacMethodology = 0
+            WHERE IsSacMethodology IS NULL;
+
+            ALTER TABLE Recommendations
+            ALTER COLUMN IsSacMethodology BIT NOT NULL;
+END
+
 
 GO -- do not remove this GO
