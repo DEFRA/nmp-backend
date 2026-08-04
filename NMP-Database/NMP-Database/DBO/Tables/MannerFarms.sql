68c55f8a91a7599d6d076298cf9490f8ddfd501e
Pooja <pooja.taneja@rsk-bsl.com>
2026-08-04 13:20:26 +0530

    in process

---
+++ NMP-Database/NMP-Database/DBO/Tables/MannerFarms.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[MannerFarms]
+(
+    ID INT IDENTITY(1,1) NOT NULL,
+    OrganisationID UNIQUEIDENTIFIER NOT NULL,
+    [Name] NVARCHAR(250) NOT NULL,
+    CountryID INT NOT NULL,
+    Postcode NVARCHAR(50) NULL,
+    AverageAnuualRainfall INT NULL,
+    RegisteredOrganicProducer BIT NOT NULL
+        CONSTRAINT DF_MannerFarms_RegisteredOrganicProducer DEFAULT (0),
+
+    CONSTRAINT [PK_MannerFarms] 
+        PRIMARY KEY CLUSTERED ([ID] ASC),
+
+    CONSTRAINT FK_MannerFarms_Countries
+        FOREIGN KEY (CountryID)
+        REFERENCES dbo.Countries(ID),
+
+    CONSTRAINT FK_MannerFarms_Organisations
+        FOREIGN KEY (OrganisationID)
+        REFERENCES dbo.Organisations(ID),
+
+    CONSTRAINT UQ_MannerFarms_Name_OrganisationID
+        UNIQUE ([Name], [OrganisationID])
+);
