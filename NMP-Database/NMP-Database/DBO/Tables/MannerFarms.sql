50145992000e381cfb4b133a56adc0cf1cb7c2a5
Pooja <pooja.taneja@rsk-bsl.com>
2026-08-06 16:33:32 +0530

    Added created on column In mannerFarms table

--- NMP-Database/NMP-Database/DBO/Tables/MannerFarms.sql
+++ NMP-Database/NMP-Database/DBO/Tables/MannerFarms.sql
@@ -6,8 +6,11 @@
     CountryID INT NOT NULL,
     Postcode NVARCHAR(50) NULL,
     AverageAnuualRainfall INT NULL,
-    RegisteredOrganicProducer BIT NOT NULL
-        CONSTRAINT DF_MannerFarms_RegisteredOrganicProducer DEFAULT (0),
+    RegisteredOrganicProducer BIT NOT NULL  CONSTRAINT DF_MannerFarms_RegisteredOrganicProducer DEFAULT (0),
+    [CreatedOn] DATETIME2 NULL CONSTRAINT DF_MannerFarms_CreatedOn DEFAULT GETDATE(), 
+    [CreatedByID] INT NULL,
+	[ModifiedOn] DATETIME2 NULL,
+	[ModifiedByID] INT NULL,
 
     CONSTRAINT [PK_MannerFarms] 
         PRIMARY KEY CLUSTERED ([ID] ASC),
@@ -21,5 +24,7 @@
         REFERENCES dbo.Organisations(ID),
 
     CONSTRAINT UQ_MannerFarms_Name_OrganisationID
-        UNIQUE ([Name], [OrganisationID])
+    UNIQUE ([Name], [OrganisationID]),
+    CONSTRAINT [FK_MannerFarms_Users_CreatedBy] FOREIGN KEY ([CreatedByID]) REFERENCES [Users]([ID]),
+    CONSTRAINT [FK_MannerFarms_Users_ModifiedBy] FOREIGN KEY ([ModifiedByID]) REFERENCES [Users]([ID])
 );
