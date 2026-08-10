a1d0ce9372ed797c69ae980a89851ce08af23e49
Pooja <pooja.taneja@rsk-bsl.com>
2026-08-10 22:59:16 +0530

    final changes for manner estimation

--- NMP-Database/NMP-Database/Script.PreDeployment.sql
+++ NMP-Database/NMP-Database/Script.PreDeployment.sql
@@ -49,193 +49,229 @@ END
 
 --04-08-2026
 
-/*====================================================*
-  Migration: Separate Farm Details into MannerFarms
-*====================================================*/
-
-DECLARE @MannerEstimationsTableName SYSNAME = N'dbo.MannerEstimations';
-DECLARE @MannerFarmsTableName SYSNAME = N'dbo.MannerFarms';
-
-
-/*====================================================*
-  MAIN MIGRATION
-*====================================================*/
-
 BEGIN TRY
 
     BEGIN TRANSACTION;
 
 
     /*====================================================
-      If MannerFarms already exists:
-      DO NOTHING
+      Table Name Constants
+    ====================================================*/
+
+    DECLARE @MannerFarmsTableName SYSNAME =
+        N'dbo.MannerFarms';
+
+    DECLARE @MannerEstimationsTableName SYSNAME =
+        N'dbo.MannerEstimations';
+
+
+    /*====================================================
+      Check if MannerFarms already exists
+
+      If it exists:
+      - Do NOTHING
+      - Do not modify MannerEstimations
+      - Do not add MannerFarmID
+      - Do not drop any columns
     ====================================================*/
 
     IF OBJECT_ID(@MannerFarmsTableName, 'U') IS NOT NULL
     BEGIN
 
         PRINT 'MannerFarms table already exists.';
-        PRINT 'Main migration skipped.';
+        PRINT 'Main migration skipped. No changes were made.';
 
     END
     ELSE
     BEGIN
 
         PRINT 'MannerFarms table does not exist.';
-        PRINT 'Starting migration...';
+        PRINT 'Starting MannerFarms migration...';
 
 
         /*====================================================
-          Entire old-column migration is Dynamic SQL.
+          Dynamic SQL
 
-          This prevents SQL Server from compiling old columns
-          when this migration is not required.
+          Old MannerEstimations columns are inside dynamic SQL
+          so SQL Server does not compile them when migration
+          is not required.
+
+          CHAR(13) + CHAR(10) is used instead of multiline
+          string literals to avoid Sonar code point 10 error.
         ====================================================*/
 
-        EXEC sys.sp_executesql
-        N'
+        DECLARE @SQL NVARCHAR(MAX);
+
+
+        SET @SQL =
+              N'CREATE TABLE dbo.MannerFarms (' + CHAR(13) + CHAR(10)
+
+            + N'    ID INT IDENTITY(1,1) PRIMARY KEY,' + CHAR(13) + CHAR(10)
+
+            + N'    OrganisationID UNIQUEIDENTIFIER NOT NULL,' + CHAR(13) + CHAR(10)
+
+            + N'    [Name] NVARCHAR(250) NOT NULL,' + CHAR(13) + CHAR(10)
+
+            + N'    CountryID INT NOT NULL,' + CHAR(13) + CHAR(10)
+
+            + N'    Postcode NVARCHAR(50) NULL,' + CHAR(13) + CHAR(10)
+
+            + N'    AverageAnuualRainfall INT NULL,' + CHAR(13) + CHAR(10)
+
+            + N'    RegisteredOrganicProducer BIT NOT NULL' + CHAR(13) + CHAR(10)
+            + N'        CONSTRAINT DF_MannerFarms_RegisteredOrganicProducer' + CHAR(13) + CHAR(10)
+            + N'        DEFAULT (0),' + CHAR(13) + CHAR(10)
+
+            + N'    [CreatedOn] DATETIME2 NULL' + CHAR(13) + CHAR(10)
+            + N'        CONSTRAINT DF_MannerFarms_CreatedOn' + CHAR(13) + CHAR(10)
+            + N'        DEFAULT GETDATE(),' + CHAR(13) + CHAR(10)
+
+            + N'    [CreatedByID] INT NULL,' + CHAR(13) + CHAR(10)
+
+            + N'    [ModifiedOn] DATETIME2 NULL,' + CHAR(13) + CHAR(10)
+
+            + N'    [ModifiedByID] INT NULL,' + CHAR(13) + CHAR(10)
+
+            + N'    CONSTRAINT FK_MannerFarms_Countries' + CHAR(13) + CHAR(10)
+            + N'        FOREIGN KEY (CountryID)' + CHAR(13) + CHAR(10)
+            + N'        REFERENCES dbo.Countries(ID),' + CHAR(13) + CHAR(10)
+
+            + N'    CONSTRAINT FK_MannerFarms_Organisations' + CHAR(13) + CHAR(10)
+            + N'        FOREIGN KEY (OrganisationID)' + CHAR(13) + CHAR(10)
+            + N'        REFERENCES dbo.Organisations(ID),' + CHAR(13) + CHAR(10)
+
+            + N'    CONSTRAINT UQ_MannerFarms_Name_OrganisationID' + CHAR(13) + CHAR(10)
+            + N'        UNIQUE ([Name], [OrganisationID]),' + CHAR(13) + CHAR(10)
+
+            + N'    CONSTRAINT FK_MannerFarms_Users_CreatedBy' + CHAR(13) + CHAR(10)
+            + N'        FOREIGN KEY ([CreatedByID])' + CHAR(13) + CHAR(10)
+            + N'        REFERENCES dbo.Users([ID]),' + CHAR(13) + CHAR(10)
+
+            + N'    CONSTRAINT FK_MannerFarms_Users_ModifiedBy' + CHAR(13) + CHAR(10)
+            + N'        FOREIGN KEY ([ModifiedByID])' + CHAR(13) + CHAR(10)
+            + N'        REFERENCES dbo.Users([ID])' + CHAR(13) + CHAR(10)
+
+            + N');';
+
+
         /*====================================================
           Step 1: Create MannerFarms
         ====================================================*/
 
-        CREATE TABLE dbo.MannerFarms
-        (
-            ID INT IDENTITY(1,1) PRIMARY KEY,
-
-            OrganisationID UNIQUEIDENTIFIER NOT NULL,
-
-            [Name] NVARCHAR(250) NOT NULL,
-
-            CountryID INT NOT NULL,
-
-            Postcode NVARCHAR(50) NULL,
-
-            AverageAnuualRainfall INT NULL,
-
-            RegisteredOrganicProducer BIT NOT NULL
-                CONSTRAINT DF_MannerFarms_RegisteredOrganicProducer
-                DEFAULT (0),
-
-            [CreatedOn] DATETIME2 NULL
-                CONSTRAINT DF_MannerFarms_CreatedOn
-                DEFAULT GETDATE(),
-
-            [CreatedByID] INT NULL,
-
-            [ModifiedOn] DATETIME2 NULL,
-
-            [ModifiedByID] INT NULL,
-
-            CONSTRAINT FK_MannerFarms_Countries
-                FOREIGN KEY (CountryID)
-                REFERENCES dbo.Countries(ID),
-
-            CONSTRAINT FK_MannerFarms_Organisations
-                FOREIGN KEY (OrganisationID)
-                REFERENCES dbo.Organisations(ID),
-
-            CONSTRAINT UQ_MannerFarms_Name_OrganisationID
-                UNIQUE ([Name], [OrganisationID]),
-
-            CONSTRAINT FK_MannerFarms_Users_CreatedBy
-                FOREIGN KEY ([CreatedByID])
-                REFERENCES dbo.Users([ID]),
-
-            CONSTRAINT FK_MannerFarms_Users_ModifiedBy
-                FOREIGN KEY ([ModifiedByID])
-                REFERENCES dbo.Users([ID])
-        );
+        EXEC sys.sp_executesql @SQL;
 
 
         /*====================================================
           Step 2: Copy Unique Farm Data
         ====================================================*/
 
-        ;WITH FarmCTE AS
-        (
-            SELECT
-                OrganisationID,
-                FarmName,
-                CountryID,
-                Postcode,
-                AverageAnuualRainfall,
-                RegisteredOrganicProducer,
-                CreatedOn,
-                CreatedByID,
+        SET @SQL =
+              N';WITH FarmCTE AS (' + CHAR(13) + CHAR(10)
 
-                ROW_NUMBER() OVER
-                (
-                    PARTITION BY OrganisationID, FarmName
-                    ORDER BY ID
-                ) AS RowNum
+            + N'    SELECT' + CHAR(13) + CHAR(10)
+            + N'        OrganisationID,' + CHAR(13) + CHAR(10)
+            + N'        FarmName,' + CHAR(13) + CHAR(10)
+            + N'        CountryID,' + CHAR(13) + CHAR(10)
+            + N'        Postcode,' + CHAR(13) + CHAR(10)
+            + N'        AverageAnuualRainfall,' + CHAR(13) + CHAR(10)
+            + N'        RegisteredOrganicProducer,' + CHAR(13) + CHAR(10)
+            + N'        CreatedOn,' + CHAR(13) + CHAR(10)
+            + N'        CreatedByID,' + CHAR(13) + CHAR(10)
 
-            FROM dbo.MannerEstimations
-        )
+            + N'        ROW_NUMBER() OVER (' + CHAR(13) + CHAR(10)
+            + N'            PARTITION BY OrganisationID, FarmName' + CHAR(13) + CHAR(10)
+            + N'            ORDER BY ID' + CHAR(13) + CHAR(10)
+            + N'        ) AS RowNum' + CHAR(13) + CHAR(10)
 
-        INSERT INTO dbo.MannerFarms
-        (
-            OrganisationID,
-            [Name],
-            CountryID,
-            Postcode,
-            AverageAnuualRainfall,
-            RegisteredOrganicProducer,
-            CreatedOn,
-            CreatedByID
-        )
-        SELECT
-            OrganisationID,
-            FarmName,
-            CountryID,
-            Postcode,
-            AverageAnuualRainfall,
-            RegisteredOrganicProducer,
-            CreatedOn,
-            CreatedByID
-        FROM FarmCTE
-        WHERE RowNum = 1;
+            + N'    FROM dbo.MannerEstimations' + CHAR(13) + CHAR(10)
+
+            + N')' + CHAR(13) + CHAR(10)
+
+            + N'INSERT INTO dbo.MannerFarms' + CHAR(13) + CHAR(10)
+            + N'(' + CHAR(13) + CHAR(10)
+            + N'    OrganisationID,' + CHAR(13) + CHAR(10)
+            + N'    [Name],' + CHAR(13) + CHAR(10)
+            + N'    CountryID,' + CHAR(13) + CHAR(10)
+            + N'    Postcode,' + CHAR(13) + CHAR(10)
+            + N'    AverageAnuualRainfall,' + CHAR(13) + CHAR(10)
+            + N'    RegisteredOrganicProducer,' + CHAR(13) + CHAR(10)
+            + N'    CreatedOn,' + CHAR(13) + CHAR(10)
+            + N'    CreatedByID' + CHAR(13) + CHAR(10)
+            + N')' + CHAR(13) + CHAR(10)
+
+            + N'SELECT' + CHAR(13) + CHAR(10)
+            + N'    OrganisationID,' + CHAR(13) + CHAR(10)
+            + N'    FarmName,' + CHAR(13) + CHAR(10)
+            + N'    CountryID,' + CHAR(13) + CHAR(10)
+            + N'    Postcode,' + CHAR(13) + CHAR(10)
+            + N'    AverageAnuualRainfall,' + CHAR(13) + CHAR(10)
+            + N'    RegisteredOrganicProducer,' + CHAR(13) + CHAR(10)
+            + N'    CreatedOn,' + CHAR(13) + CHAR(10)
+            + N'    CreatedByID' + CHAR(13) + CHAR(10)
+
+            + N'FROM FarmCTE' + CHAR(13) + CHAR(10)
+            + N'WHERE RowNum = 1;';
+
+
+        EXEC sys.sp_executesql @SQL;
 
 
         /*====================================================
           Step 3: Add MannerFarmID
         ====================================================*/
 
-        ALTER TABLE dbo.MannerEstimations
-        ADD MannerFarmID INT NULL;
+        SET @SQL =
+            N'ALTER TABLE dbo.MannerEstimations ' +
+            N'ADD MannerFarmID INT NULL;';
+
+        EXEC sys.sp_executesql @SQL;
 
 
         /*====================================================
           Step 4: Update MannerFarmID
         ====================================================*/
 
-        UPDATE ME
-        SET MannerFarmID = MF.ID
+        SET @SQL =
+              N'UPDATE ME' + CHAR(13) + CHAR(10)
+            + N'SET MannerFarmID = MF.ID' + CHAR(13) + CHAR(10)
 
-        FROM dbo.MannerEstimations AS ME
+            + N'FROM dbo.MannerEstimations AS ME' + CHAR(13) + CHAR(10)
 
-        INNER JOIN dbo.MannerFarms AS MF
-            ON MF.OrganisationID = ME.OrganisationID
-           AND MF.[Name] = ME.FarmName;
+            + N'INNER JOIN dbo.MannerFarms AS MF' + CHAR(13) + CHAR(10)
+            + N'    ON MF.OrganisationID = ME.OrganisationID' + CHAR(13) + CHAR(10)
+            + N'   AND MF.[Name] = ME.FarmName;';
+
+
+        EXEC sys.sp_executesql @SQL;
 
 
         /*====================================================
           Step 5: Check Unmapped Records
         ====================================================*/
 
-        DECLARE @UnmappedCount INT;
+        DECLARE @UnmappedCount INT = 0;
 
-        SELECT @UnmappedCount = COUNT(*)
-        FROM dbo.MannerEstimations
-        WHERE MannerFarmID IS NULL;
+
+        SET @SQL =
+              N'SELECT @Count = COUNT(*)' + CHAR(13) + CHAR(10)
+            + N'FROM dbo.MannerEstimations' + CHAR(13) + CHAR(10)
+            + N'WHERE MannerFarmID IS NULL;';
+
+
+        EXEC sys.sp_executesql
+            @SQL,
+            N'@Count INT OUTPUT',
+            @Count = @UnmappedCount OUTPUT;
 
 
         IF @UnmappedCount > 0
         BEGIN
 
-            PRINT ''Migration failed.'';
-
-            PRINT ''Unmapped records: ''
+            PRINT 'Migration failed.';
+            PRINT 'Unmapped records: '
                 + CAST(@UnmappedCount AS VARCHAR(20));
+
+            ROLLBACK TRANSACTION;
 
             RETURN;
 
@@ -246,31 +282,38 @@ BEGIN TRY
           Step 6: Make MannerFarmID NOT NULL
         ====================================================*/
 
-        ALTER TABLE dbo.MannerEstimations
-        ALTER COLUMN MannerFarmID INT NOT NULL;
+        SET @SQL =
+            N'ALTER TABLE dbo.MannerEstimations ' +
+            N'ALTER COLUMN MannerFarmID INT NOT NULL;';
+
+        EXEC sys.sp_executesql @SQL;
 
 
         /*====================================================
           Step 7: Create Foreign Key
         ====================================================*/
 
-        ALTER TABLE dbo.MannerEstimations
-        ADD CONSTRAINT FK_MannerEstimations_MannerFarms
-            FOREIGN KEY (MannerFarmID)
-            REFERENCES dbo.MannerFarms(ID);
+        SET @SQL =
+              N'ALTER TABLE dbo.MannerEstimations' + CHAR(13) + CHAR(10)
+            + N'ADD CONSTRAINT FK_MannerEstimations_MannerFarms' + CHAR(13) + CHAR(10)
+            + N'    FOREIGN KEY (MannerFarmID)' + CHAR(13) + CHAR(10)
+            + N'    REFERENCES dbo.MannerFarms(ID);';
+
+
+        EXEC sys.sp_executesql @SQL;
 
 
         /*====================================================
           Step 8: Create Unique Constraint
         ====================================================*/
 
-        ALTER TABLE dbo.MannerEstimations
-        ADD CONSTRAINT UQ_MannerEstimations_Name_MannerFarmID
-            UNIQUE
-            (
-                [Name],
-                MannerFarmID
-            );
+        SET @SQL =
+              N'ALTER TABLE dbo.MannerEstimations' + CHAR(13) + CHAR(10)
+            + N'ADD CONSTRAINT UQ_MannerEstimations_Name_MannerFarmID' + CHAR(13) + CHAR(10)
+            + N'    UNIQUE ([Name], MannerFarmID);';
+
+
+        EXEC sys.sp_executesql @SQL;
 
 
         /*====================================================
@@ -282,15 +325,17 @@ BEGIN TRY
             SELECT 1
             FROM sys.default_constraints
             WHERE name =
-                ''DF_MannerEstimations_RegisteredOrganicProducer''
+                'DF_MannerEstimations_RegisteredOrganicProducer'
               AND parent_object_id =
-                OBJECT_ID(''dbo.MannerEstimations'')
+                OBJECT_ID(@MannerEstimationsTableName)
         )
         BEGIN
 
-            ALTER TABLE dbo.MannerEstimations
-            DROP CONSTRAINT
-                DF_MannerEstimations_RegisteredOrganicProducer;
+            SET @SQL =
+                N'ALTER TABLE dbo.MannerEstimations ' +
+                N'DROP CONSTRAINT DF_MannerEstimations_RegisteredOrganicProducer;';
+
+            EXEC sys.sp_executesql @SQL;
 
         END;
 
@@ -303,15 +348,18 @@ BEGIN TRY
         (
             SELECT 1
             FROM sys.foreign_keys
-            WHERE name = ''FK_MannerEstimations_Countries''
+            WHERE name =
+                'FK_MannerEstimations_Countries'
               AND parent_object_id =
-                OBJECT_ID(''dbo.MannerEstimations'')
+                OBJECT_ID(@MannerEstimationsTableName)
         )
         BEGIN
 
-            ALTER TABLE dbo.MannerEstimations
-            DROP CONSTRAINT
-                FK_MannerEstimations_Countries;
+            SET @SQL =
+                N'ALTER TABLE dbo.MannerEstimations ' +
+                N'DROP CONSTRAINT FK_MannerEstimations_Countries;';
+
+            EXEC sys.sp_executesql @SQL;
 
         END;
 
@@ -324,15 +372,18 @@ BEGIN TRY
         (
             SELECT 1
             FROM sys.foreign_keys
-            WHERE name = ''FK_MannerEstimations_Organisations''
+            WHERE name =
+                'FK_MannerEstimations_Organisations'
               AND parent_object_id =
-                OBJECT_ID(''dbo.MannerEstimations'')
+                OBJECT_ID(@MannerEstimationsTableName)
         )
         BEGIN
 
-            ALTER TABLE dbo.MannerEstimations
-            DROP CONSTRAINT
-                FK_MannerEstimations_Organisations;
+            SET @SQL =
+                N'ALTER TABLE dbo.MannerEstimations ' +
+                N'DROP CONSTRAINT FK_MannerEstimations_Organisations;';
+
+            EXEC sys.sp_executesql @SQL;
 
         END;
 
@@ -346,15 +397,17 @@ BEGIN TRY
             SELECT 1
             FROM sys.key_constraints
             WHERE name =
-                ''UQ_MannerEstimations_Name_OrganisationID''
+                'UQ_MannerEstimations_Name_OrganisationID'
               AND parent_object_id =
-                OBJECT_ID(''dbo.MannerEstimations'')
+                OBJECT_ID(@MannerEstimationsTableName)
         )
         BEGIN
 
-            ALTER TABLE dbo.MannerEstimations
-            DROP CONSTRAINT
-                UQ_MannerEstimations_Name_OrganisationID;
+            SET @SQL =
+                N'ALTER TABLE dbo.MannerEstimations ' +
+                N'DROP CONSTRAINT UQ_MannerEstimations_Name_OrganisationID;';
+
+            EXEC sys.sp_executesql @SQL;
 
         END;
 
@@ -363,31 +416,34 @@ BEGIN TRY
           Step 13: Drop Old Farm Columns
         ====================================================*/
 
-        ALTER TABLE dbo.MannerEstimations
-        DROP COLUMN
-            OrganisationID,
-            FarmName,
-            CountryID,
-            Postcode,
-            AverageAnuualRainfall,
-            RegisteredOrganicProducer;
+        SET @SQL =
+              N'ALTER TABLE dbo.MannerEstimations' + CHAR(13) + CHAR(10)
+            + N'DROP COLUMN' + CHAR(13) + CHAR(10)
+            + N'    OrganisationID,' + CHAR(13) + CHAR(10)
+            + N'    FarmName,' + CHAR(13) + CHAR(10)
+            + N'    CountryID,' + CHAR(13) + CHAR(10)
+            + N'    Postcode,' + CHAR(13) + CHAR(10)
+            + N'    AverageAnuualRainfall,' + CHAR(13) + CHAR(10)
+            + N'    RegisteredOrganicProducer;';
 
 
-        PRINT ''MannerFarms migration completed successfully.'';
+        EXEC sys.sp_executesql @SQL;
 
-        ';
+
+        PRINT 'MannerFarms migration completed successfully.';
 
     END;
 
 
     /*====================================================
-      Commit
+      Commit Transaction
     ====================================================*/
 
     IF @@TRANCOUNT > 0
     BEGIN
         COMMIT TRANSACTION;
     END;
+
 
 END TRY
 
@@ -398,10 +454,13 @@ BEGIN CATCH
         ROLLBACK TRANSACTION;
     END;
 
+
     PRINT 'Migration failed.';
     PRINT ERROR_MESSAGE();
 
 END CATCH;
+
+GO
 
 
 GO
